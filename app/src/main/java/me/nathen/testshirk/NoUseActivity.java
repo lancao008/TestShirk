1279f9d4f866edc100a79f7ef389686afdcdd3a7
Pan.Li <lipan@jiecao.fm>
2015-04-17 16:38:29 +0800

    test activity in manifest

---
+++ app/src/main/java/me/nathen/testshirk/NoUseActivity.java
@@ -0,0 +1,16 @@
+package me.nathen.testshirk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Nathen on 15/4/17.
+ */
+public class NoUseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nouse_activity);
+
+    }
+}
